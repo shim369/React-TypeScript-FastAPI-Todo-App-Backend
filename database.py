2fede955a79a91df7eec54cf167c32f92f06be84
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-05-31 16:46:55 +0900

    update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from decouple import config
+from typing import Union
 import motor.motor_asyncio
 
 MONOGO_API_KEY = config('MONOGO_API_KEY')
@@ -7,3 +8,17 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONOGO_API_KEY)
 database = client.API_DB
 collection_todo = database.todo
 collection_user = database.user
+
+def todo_serializer(todo) -> dict:
+    return {
+        "id": str(todo["_id"]),
+        "title": todo["title"],
+        "description": todo["description"],
+    }
+
+async def db_create_todo(data: dict) -> Union[dict, bool]:
+    todo = await collection_todo.insert_one(data)
+    new_todo = await collection_todo.find_one({"_id": todo.inserted_id})
+    if new_todo:
+        return todo_serializer(new_todo)
+    return False
