c7321798d35738c6f4ca8eee9bd2669e90b5aa46
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-01 19:41:48 +0900

    update database.py

--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+from fastapi import HTTPException
 from decouple import config
 from typing import Union
 import motor.motor_asyncio
 from bson import ObjectId
+from auth_utils import AuthJwtCsrf
+
 
 MONOGO_API_KEY = config('MONOGO_API_KEY')
 
@@ -9,12 +12,19 @@ client = motor.motor_asyncio.AsyncIOMotorClient(MONOGO_API_KEY)
 database = client.API_DB
 collection_todo = database.todo
 collection_user = database.user
+auth = AuthJwtCsrf()
 
 def todo_serializer(todo) -> dict:
     return {
         "id": str(todo["_id"]),
         "title": todo["title"],
         "description": todo["description"],
+    }
+
+def user_serializer(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "email": user["email"],
     }
 
 async def db_create_todo(data: dict) -> Union[dict, bool]:
@@ -55,3 +65,26 @@ async def db_delete_todo(id: str) -> bool:
         if (deleted_todo.deleted_count > 0):
             return True
     return False
+
+async def db_signup(data: dict) -> dict:
+    email = data.get("email")
+    password = data.get("password")
+    overlap_user = await collection_user.find_one({"email": email})
+    if overlap_user:
+        raise HTTPException(status_code=400, detail='Email is already taken')
+    if not password or len(password) < 6:
+        raise HTTPException(status_code=400, detail='Password too short')
+    user = await collection_user.insert_one({"email": email, "password": auth.generate_hashed_pw(password)})
+    new_user = await collection_user.find_one({"_id": user.inserted_id})
+    return user_serializer(new_user)
+
+async def db_login(data: dict) -> str:
+    email = data.get("email")
+    password = data.get("password")
+    user = await collection_user.find_one({"email": email})
+    if not user or not auth.verify_pw(password, user["password"]):
+        raise HTTPException(
+            status_code=401, detail='Invalid email or password'
+        )
+    token = auth.encode_jwt(user['email'])
+    return token
