e8b64772caeb38b97981a58c1f75a50dbfd4b595
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-01 19:42:07 +0900

    update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-
+from typing import Optional
 
 class Todo(BaseModel):
     id: str
@@ -12,3 +12,11 @@ class TodoBody(BaseModel):
 
 class SuccessMsg(BaseModel):
     message: str
+
+class UserBody(BaseModel):
+    email: str
+    password: str
+
+class UserInfo(BaseModel):
+    id: Optional[str] = None
+    email: str
