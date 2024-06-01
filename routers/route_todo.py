20797b1c954d81c5431101d7d3ac841606b6e817
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-06-01 10:07:44 +0900

    update router

--- routers/route_todo.py
+++ routers/route_todo.py
@@ -7,7 +7,7 @@ from starlette.status import HTTP_201_CREATED
 
 router = APIRouter()
 
-@router.post("api/todo", response_model=Todo)
+@router.post("/api/todo", response_model=Todo)
 async def create_todo(request: Request, response: Response, data: TodoBody):
     todo = jsonable_encoder(data)
     res = await db_create_todo(todo)
